fb69a89046ded69126373ac219aa2b59415ee918
Max Magnusson <maxerik453@gmail.com>
2023-01-23 11:26:39 +0100

    La in lite i views

---
+++ databaser/Assignment 1/views.sql
@@ -0,0 +1,44 @@
+/*BasicInformation(idnr, name, login, program, branch)
+
+FinishedCourses(student, course, grade, credits)
+
+PassedCourses(student, course, credits)
+
+Registrations(student, course, status)
+
+UnreadMandatory(student, course)
+
+PathToGraduation(student, totalCredits, mandatoryLeft, mathCredits, researchCredits, seminarCourses, qualified)
+
+ALL VIEWS
+*/
+
+CREATE VIEW BasicInformation AS
+SELECT idnr, name, login, program, branch
+FROM Students, Studentbranches
+WHERE condition???; 
+
+CREATE VIEW FinishedCourses AS
+SELECT student, course, grade, credits
+FROM Taken, Courses
+WHERE condition???;
+
+CREATE VIEW PassedCourses AS
+SELECT student, course, credits
+FROM FinishedCourses
+WHERE condition???;
+
+CREATE VIEW Registrations AS
+SELECT student, course, status
+FROM Registered /*status är jag fundersam över*/
+WHERE condition???;
+
+CREATE VIEW UnreadMandatory AS
+SELECT student, course
+FROM /*osäker*/
+WHERE condition???;
+
+CREATE VIEW PathToGraduation AS
+SELECT /*osäker*/
+FROM /*osäker*/
+WHERE /*osäker*/;
