4ec1d5bca049bd44cabd4ab9ceec85a86104941d
Max Magnusson <maxerik453@gmail.com>
2023-02-27 20:23:35 +0100

    Triggers fixade för prerequisitecourses och waitinglist position fixad

--- databaser/Assignment 3/tests.sql
+++ databaser/Assignment 3/tests.sql
@@ -1,3 +1,4 @@
----INSERT INTO Registrations VALUES ('2222222222','CCC222');
 INSERT INTO Registrations VALUES ('2222222222', 'CCC555');
-INSERT INTO Registrations VALUES ('1111111111', 'CCC555');
+INSERT INTO Registrations VALUES ('6666666666', 'CCC222');
+SELECT * FROM WaitingList order by position
+
